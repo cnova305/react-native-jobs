70174b06ad809505ad9f7c4801b4119911719ae5
Christopher Juwao <njuwao@icloud.com>
2023-04-19 01:15:32 +0200

    Initial commit

---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { useRouter } from "expo-router";
+
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import { PopularjobCard } from "../../common/cards/popular/PopularJobCard";
+
+const Popularjobs = () => {
+  const router = useRouter();
+  const isLoading = false;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}></View>
+    </View>
+  );
+};
+
+export default Popularjobs;
